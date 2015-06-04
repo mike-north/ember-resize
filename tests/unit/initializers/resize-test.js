5f0bfe61394eb39405d7f73ca21827d0e0966bed
Mike North <northm@yahoo-inc.com>
2015-06-04 13:38:26 -0700

    Intializer tests

--- tests/unit/initializers/resize-test.js
+++ tests/unit/initializers/resize-test.js
@@ -5,6 +5,7 @@ import { module, test } from 'qunit';
 let container, application;
 
 module('Unit | Initializer | resize', {
+  needs: ['service:resize'],
   beforeEach() {
     Ember.run(function() {
       application = Ember.Application.create();
@@ -14,10 +15,21 @@ module('Unit | Initializer | resize', {
   }
 });
 
-// Replace this with your real tests.
-test('it works', function(assert) {
+test('service is registered to the container', function(assert) {
+  assert.ok(!application.__container__.lookup('service:resize'), 'not registered as service:resize in the container before initializer');
   initialize(container, application);
-
-  // you would normally confirm the results of the initializer here
-  assert.ok(true);
+  assert.ok(application.__container__.lookup('service:resize'), 'registered as service:resize in the container after initializer');
 });
+
+test('service configuration is registered to the container', function(assert) {
+  assert.ok(!application.__container__.lookup('config:resize-service'), 'not registered as config:resize-service in the container before initializer');
+  initialize(container, application);
+  assert.ok(application.__container__.lookup('config:resize-service'), 'registered as config:resize-service in the container after initializer');
+});
+
+test('service configuration is injected onto the resize service', function(assert) {
+  initialize(container, application);
+  let resize = application.__container__.lookup('service:resize');
+  assert.deepEqual(resize.get('resizeServiceDefaults'), application.__container__.lookup('config:resize-service'), 'defaults are registered to service as "resizeServiceDefaults"');
+});
+
