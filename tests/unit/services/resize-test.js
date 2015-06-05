cb4b95d17b1f1baed528c33cfc9beb5bf4938761
Mike North <northm@yahoo-inc.com>
2015-06-04 23:17:05 -0700

    IE10/11 Support

--- tests/unit/services/resize-test.js
+++ tests/unit/services/resize-test.js
@@ -17,16 +17,18 @@ test('it fires "didResize"  when the window is resized', function(assert) {
     didResizeCallCount++;
   });
 
-  let evt = new window.Event('resize');
+  Ember.$(window).trigger('resize');
 
-  window.dispatchEvent(evt);
-  assert.equal(didResizeCallCount, 1, 'didResize called 1 time on event firing');
   service.incrementProperty('_oldHeight', -20);
-  window.dispatchEvent(evt);
+
+  Ember.$(window).trigger('resize');
+
   assert.equal(didResizeCallCount, 2, 'didResize called another time on event firing again');
   service.set('heightSensitive', false);
   service.incrementProperty('_oldHeight', -20);
-  window.dispatchEvent(evt);
+
+  Ember.$(window).trigger('resize');
+
   assert.equal(didResizeCallCount, 2, 'didResize shouldn\'t be called again if heightSensitive is false');
 
 });
@@ -45,11 +47,10 @@ test('it fires "debouncedDidResize"  when the window is resized', function(asser
     debouncedDidResizeCallCount++;
   });
 
-  let evt = new window.Event('resize');
   let evtCount = 0;
 
   function triggerEvent() {
-    window.dispatchEvent(evt);
+    Ember.$(window).trigger('resize');
   }
 
   for (let i = 0; i < 6; i++) {
@@ -57,9 +58,8 @@ test('it fires "debouncedDidResize"  when the window is resized', function(asser
   }
 
   assert.equal(debouncedDidResizeCallCount, 0, 'debouncedDidResize not called yet');
-  // Ember.run.later(() => {
-  //   assert.equal(debouncedDidResizeCallCount, 1, 'debouncedDidResize called 1 time after 500ms');
-  //   QUnit.start();
-  // }, 500);
-  QUnit.start();
+  Ember.run.later(() => {
+    assert.equal(debouncedDidResizeCallCount, 1, 'debouncedDidResize called 1 time after 500ms');
+    QUnit.start();
+  }, 500);
 });
