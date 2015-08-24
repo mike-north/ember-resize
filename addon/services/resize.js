c3892bc8594ad00ffa0fe70fa0313cd7ce6b5d99
Mike North <northm@yahoo-inc.com>
2015-08-24 10:59:04 -0700

    Style fixes

--- addon/services/resize.js
+++ addon/services/resize.js
@@ -46,11 +46,12 @@ export default Base.extend(Evented, {
   },
 
   _updateCachedWindowSize(w, h, debounced=false) {
-
-    this.setProperties({
-      [`_oldWidth${debounced ? 'Debounced' : ''}`]: w,
-      [`_oldHeight${debounced ? 'Debounced' : ''}`]: h
-    });
+    const wKey = `_oldWidth${debounced ? 'Debounced' : ''}`;
+    const hKey = `_oldHeight${debounced ? 'Debounced' : ''}`;
+    let props = {};
+    props[wKey] = w;
+    props[hKey] = h;
+    this.setProperties(props);
   },
 
   _installResizeListener() {
@@ -62,14 +63,14 @@ export default Base.extend(Evented, {
   },
 
   _fireResizeNotification(evt) {
-    const {innerWidth, innerHeight} = window;
+    const { innerWidth, innerHeight } = window;
     if (this._hasWindowSizeChanged(innerWidth, innerHeight)) {
       this.trigger('didResize', evt);
       this._updateCachedWindowSize(innerWidth, innerHeight);
     }
   },
   _fireDebouncedResizeNotification(evt) {
-    const {innerWidth, innerHeight} = window;
+    const { innerWidth, innerHeight } = window;
     if (this._hasWindowSizeChanged(innerWidth, innerHeight, true)) {
       this.trigger('debouncedDidResize', evt);
       this._updateCachedWindowSize(innerWidth, innerHeight, true);
